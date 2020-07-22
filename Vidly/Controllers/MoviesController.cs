c4838b3959a5310c4bde296752632c982eea452a
sdimons <sdimons@gmail.com>
2020-07-22 15:52:47 +0300

    Fill movies in the private block

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -10,6 +10,19 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        private List<Movie> movies = new List<Movie>
+        {
+            new Movie
+                {
+                    Id = 1,
+                    Name = "Shrek"
+                },
+                new Movie
+                {
+                    Id = 2,
+                    Name = "Wall-e"
+                }
+        };
         // GET: Movies/Random
         public ActionResult Random()
         {
@@ -56,17 +69,6 @@ namespace Vidly.Controllers
         //}
         public ActionResult Index()
         {
-            var movies = new List<Movie>
-            {
-                new Movie
-                {
-                    Name = "Shrek"
-                },
-                new Movie
-                {
-                    Name = "Wall-e"
-                }
-            };
             var viewModel = new MoviesViewModel
             {
                 Movies = movies
