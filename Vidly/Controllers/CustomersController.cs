3fc675d73958251c596cdd042fb8646541e454d5
sdimons <sdimons@gmail.com>
2020-08-19 19:31:01 +0300

    Data Cache

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Runtime.Caching;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
@@ -84,6 +85,11 @@ namespace Vidly.Controllers
             //var customers = GetCustomers();
             //var customers = _context.Customers.Include(c => c.MembershipType).ToList();
             //return View(customers);
+            if (MemoryCache.Default["Genres"] == null)
+            {
+                MemoryCache.Default["Genres"] = _context.Genres.ToList();
+            }
+            var genres = MemoryCache.Default["Genres"] as IEnumerable<Genre>;
             return View();
         }
 
--- Vidly/Readme/9_Performance_Optimization.md
+++ Vidly/Readme/9_Performance_Optimization.md
@@ -53,3 +53,17 @@ public ActionResult Index()
     return View();
 }
 ```
+
+## Data Cache
+CustomersController:
+```
+public ActionResult Index()
+{
+    if (MemoryCache.Default["Genres"] == null)
+    {
+        MemoryCache.Default["Genres"] = _context.Genres.ToList();
+    }
+    var genres = MemoryCache.Default["Genres"] as IEnumerable<Genre>;
+    return View();
+}
+```
