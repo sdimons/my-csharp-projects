d76bdfa1324ba4d61e9bc59bd7bfdf9195876147
sdimons <sdimons@gmail.com>
2020-12-03 23:52:20 +0300

    Added FunWithMethods

--- pro-csharp-7-master/Chapter_4/FunWithMethods/FunWithMethods/Program.cs
+++ pro-csharp-7-master/Chapter_4/FunWithMethods/FunWithMethods/Program.cs
@@ -63,12 +63,12 @@ namespace FunWithMethods
             #endregion
 
             #region Param array
-            // Pass in a comma-delimited list of doubles...
+            // Передать список значений double, разделенные запятыми
             double average;
             average = CalculateAverage(4.0, 3.2, 5.7, 64.22, 87.2);
             Console.WriteLine("Average of data is: {0}", average);
 
-            // ...or pass an array of doubles.
+            // ... или передать массив значений double
             double[] data = {4.0, 3.2, 5.7};
             average = CalculateAverage(data);
             Console.WriteLine("Average of data is: {0}", average);
@@ -84,15 +84,16 @@ namespace FunWithMethods
             EnterLogData("Oh no! I can't find the payroll data", "CFO");
             Console.WriteLine();
 
-            DisplayFancyMessage(message: "Wow!  Very Fancy indeed!",
+            DisplayFancyMessage(
+                message: "Wow!  Very Fancy indeed!",
                 textColor: ConsoleColor.DarkRed,
                 backgroundColor: ConsoleColor.White);
 
-            DisplayFancyMessage(backgroundColor: ConsoleColor.Green,
+            DisplayFancyMessage(
+                backgroundColor: ConsoleColor.Green,
                 message: "Testing...",
                 textColor: ConsoleColor.DarkBlue);
 
-            // This is OK, as positional args are listed before named args.
             DisplayFancyMessage(ConsoleColor.Blue,
                 message: "Testing...",
                 backgroundColor: ConsoleColor.White);
@@ -110,10 +111,6 @@ namespace FunWithMethods
         static int Add(int x, int y)
         {
             int ans = x + y;
-
-            // Caller will not see these changes
-            // as you are modifying a copy of the
-            // original data.
             x = 10000;
             y = 88888;
             return ans;
@@ -138,7 +135,8 @@ namespace FunWithMethods
 
         #region Ref parameters
         // Reference parameters.
-        public static void SwapStrings(ref string s1, ref string s2)
+        public static void SwapStrings
+            (ref string s1, ref string s2)
         {
             string tempStr = s1;
             s1 = s2;
@@ -169,42 +167,45 @@ namespace FunWithMethods
 
         #region Params array
         // Return average of "some number" of doubles.
-        static double CalculateAverage(params double[] values)
+        static double CalculateAverage
+            (params double[] values)
         {
-            Console.WriteLine("You sent me {0} doubles.", values.Length);
-
             double sum = 0;
             if (values.Length == 0)
                 return sum;
 
             for (int i = 0; i < values.Length; i++)
                 sum += values[i];
-            return (sum / values.Length);
+            return 
+                (sum / values.Length);
         }
         #endregion
 
         #region Optional arguments
-        static void EnterLogData(string message, string owner = "Programmer")
+        static void EnterLogData(string message, 
+            string owner = "Programmer")
         {
             Console.Beep();
             Console.WriteLine("Error: {0}", message);
-            Console.WriteLine("Owner of Error: {0}", owner);
+            Console.WriteLine("Owner of Error: {0}", 
+                owner);
         }
 
-        static void DisplayFancyMessage(ConsoleColor textColor = ConsoleColor.Blue,
-            ConsoleColor backgroundColor = ConsoleColor.White, string message = "Test Message")
+        static void DisplayFancyMessage(
+            ConsoleColor textColor = ConsoleColor.Blue,
+            ConsoleColor backgroundColor = ConsoleColor.White, 
+            string message = "Test Message")
         {
-            // Store old colors to restore once message is printed. 
-            ConsoleColor oldTextColor = Console.ForegroundColor;
-            ConsoleColor oldbackgroundColor = Console.BackgroundColor;
+            ConsoleColor oldTextColor = 
+                Console.ForegroundColor;
+            ConsoleColor oldbackgroundColor = 
+                Console.BackgroundColor;
 
-            // Set new colors and print message.
             Console.ForegroundColor = textColor;
             Console.BackgroundColor = backgroundColor;
 
             Console.WriteLine(message);
 
-            // Restore previous colors. 
             Console.ForegroundColor = oldTextColor;
             Console.BackgroundColor = oldbackgroundColor;
         }
