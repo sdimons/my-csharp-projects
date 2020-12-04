4566c0de827a053d07ac2590722d2b230ac06475
sdimons <sdimons@gmail.com>
2020-12-05 00:28:23 +0300

    Added FunWithStructures

--- pro-csharp-7-master/Chapter_4/FunWithStructures/FunWithStructures/Program.cs
+++ pro-csharp-7-master/Chapter_4/FunWithStructures/FunWithStructures/Program.cs
@@ -8,29 +8,15 @@ namespace FunWithStructures
     #region Simple structure
     struct Point
     {
-        // Fields of the structure.
         public int X;
         public int Y;
 
-        // Add 1 to the (X, Y) position.
-        public void Increment()
-        {
-            X++; Y++;
-        }
-
-        // Subtract 1 from the (X, Y) position.
-        public void Decrement()
-        {
-            X--; Y--;
-        }
-
-        // Display the current position.
         public void Display()
         {
-            Console.WriteLine("X = {0}, Y = {1}", X, Y);
+            Console.WriteLine(
+                "X = {0}, Y = {1}", 
+                X, Y);
         }
-
-        // A custom constructor.
         public Point(int XPos, int YPos)
         {
             X = XPos;
@@ -51,7 +37,7 @@ namespace FunWithStructures
             myPoint.Display();
 
             // Adjust the X and Y values.
-            myPoint.Increment();
+            //myPoint.Increment();
             myPoint.Display();
 
             // Set all fields to default values
