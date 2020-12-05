614a0a482132b4f2f959ad2a73497cbab18af4a1
sdimons <sdimons@gmail.com>
2020-12-05 21:12:40 +0300

    Added ValueAndReferenceTypes

--- pro-csharp-7-master/Chapter_4/ValueAndReferenceTypes/ValueAndReferenceTypes/Program.cs
+++ pro-csharp-7-master/Chapter_4/ValueAndReferenceTypes/ValueAndReferenceTypes/Program.cs
@@ -8,29 +8,15 @@ namespace ValueAndReferenceTypes
     #region Simple structure
     struct Point
     {
-        // Fields of the structure.
         public int X;
         public int Y;
 
-        // Add 1 to the (X, Y) position.
-        public void Increment()
-        {
-            X++; Y++;
-        }
-
-        // Subtract 1 from the (X, Y) position.
-        public void Decrement()
-        {
-            X--; Y--;
-        }
-
-        // Display the current position.
         public void Display()
         {
-            Console.WriteLine("X = {0}, Y = {1}", X, Y);
+            Console.WriteLine
+                ("X = {0}, Y = {1}", X, Y);
         }
 
-        // A custom constructor.
         public Point(int XPos, int YPos)
         {
             X = XPos;
@@ -42,29 +28,15 @@ namespace ValueAndReferenceTypes
     #region Simple class
     class PointRef
     {
-        // Fields of the structure.
         public int X;
         public int Y;
 
-        // Add 1 to the (X, Y) position.
-        public void Increment()
-        {
-            X++; Y++;
-        }
-
-        // Subtract 1 from the (X, Y) position.
-        public void Decrement()
-        {
-            X--; Y--;
-        }
-
-        // Display the current position.
         public void Display()
         {
-            Console.WriteLine("X = {0}, Y = {1}", X, Y);
+            Console.WriteLine
+                ("X = {0}, Y = {1}", X, Y);
         }
 
-        // A custom constructor.
         public PointRef(int XPos, int YPos)
         {
             X = XPos;
@@ -83,12 +55,12 @@ namespace ValueAndReferenceTypes
 
     struct Rectangle
     {
-        // The Rectangle structure contains a reference type member.
         public ShapeInfo RectInfo;
 
         public int RectTop, RectLeft, RectBottom, RectRight;
 
-        public Rectangle(string info, int top, int left, int bottom, int right)
+        public Rectangle(string info, int top, int left, 
+            int bottom, int right)
         {
             RectInfo = new ShapeInfo(info);
             RectTop = top; RectBottom = bottom;
@@ -99,7 +71,8 @@ namespace ValueAndReferenceTypes
         {
             Console.WriteLine("String = {0}, Top = {1}, Bottom = {2}, " +
                               "Left = {3}, Right = {4}",
-                RectInfo.InfoString, RectTop, RectBottom, RectLeft, RectRight);
+                RectInfo.InfoString, RectTop, RectBottom, 
+                RectLeft, RectRight);
         }
     }
     #endregion
@@ -129,11 +102,9 @@ namespace ValueAndReferenceTypes
             Point p1 = new Point(10, 10);
             Point p2 = p1;
 
-            // Print both points.
             p1.Display();
             p2.Display();
 
-            // Change p1.X and print again. p2.X is not changed.
             p1.X = 100;
             Console.WriteLine("\n=> Changed p1.X\n");
             p1.Display();
@@ -144,15 +115,12 @@ namespace ValueAndReferenceTypes
         #region Classes and assignment operator
         static void ReferenceTypeAssignment()
         {
-            Console.WriteLine("Assigning reference types\n");
             PointRef p1 = new PointRef(10, 10);
             PointRef p2 = p1;
 
-            // Print both point refs.
             p1.Display();
             p2.Display();
 
-            // Change p1.X and print again.
             p1.X = 100;
             Console.WriteLine("\n=> Changed p1.X\n");
             p1.Display();
@@ -181,5 +149,11 @@ namespace ValueAndReferenceTypes
             r2.Display();
         }
         #endregion
+
+        static void LocalValueTypes()
+        {
+            int i = 0; //структура System.Int32
+            Point p = new Point(); // Структура Point
+        } // Здесь i и p покидают стек!
     }
 }
